3f840ad3094fd20952a7ceb52a7859054dcd3999
LC <libertycodeg@gmail.com>
2024-11-26 19:50:38 +0300

    cr chTskTitl, cr chTskSts

--- src/model/tasks-reducer/tasks-reducer.test.ts
+++ src/model/tasks-reducer/tasks-reducer.test.ts
@@ -1,6 +1,6 @@
 
 import { TasksStateType } from '@/app/App/App';
-import { addTaskAC, removeTaskAC, tasksReducer } from './tasks-reducer';
+import { addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer } from './tasks-reducer';
 
 test('correct task should be deleted from correct array', () => {
   const startState: TasksStateType = {
@@ -55,4 +55,59 @@ test('correct task should be added to correct array', () => {
   expect(endState['todolistId2'][0].id).toBeDefined();
   expect(endState['todolistId2'][0].title).toBe('bread');
   expect(endState['todolistId2'][0].isDone).toBe(false);
+});
+
+test('status of specified task should be changed', () => {
+  const startState: TasksStateType = {
+    todolistId1: [
+      { id: '1', title: 'CSS', isDone: false },
+      { id: '2', title: 'JS', isDone: true },
+      { id: '3', title: 'React', isDone: false },
+    ],
+    todolistId2: [
+      { id: '1', title: 'bread', isDone: false },
+      { id: '2', title: 'milk', isDone: true },
+      { id: '3', title: 'tea', isDone: false },
+    ],
+  };
+
+  const endState = tasksReducer(
+    startState,
+    changeTaskStatusAC({
+      taskId: '2',
+      isDone: false,
+      todolistId: 'todolistId2',
+    })
+  );
+
+  expect(endState['todolistId2'][1]).toBeDefined();
+  expect(endState['todolistId2'][1].isDone).toBe(false);
+
+});
+
+test('title of specified task should be changed', () => {
+  const startState: TasksStateType = {
+    todolistId1: [
+      { id: '1', title: 'CSS', isDone: false },
+      { id: '2', title: 'JS', isDone: true },
+      { id: '3', title: 'React', isDone: false },
+    ],
+    todolistId2: [
+      { id: '1', title: 'bread', isDone: false },
+      { id: '2', title: 'milk', isDone: true },
+      { id: '3', title: 'tea', isDone: false },
+    ],
+  };
+
+  const endState = tasksReducer(
+    startState,
+    changeTaskTitleAC({
+      taskId: '2',
+      title: "New title",
+      todolistId: 'todolistId2',
+    })
+  );
+
+  expect(endState['todolistId2'][2]).toBeDefined();
+  expect(endState['todolistId2'][1].title).toBe("New title");
 });
--- src/model/tasks-reducer/tasks-reducer.ts
+++ src/model/tasks-reducer/tasks-reducer.ts
@@ -9,6 +9,13 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
     case "ADD-TASK": {
       return { ...state, [action.payload.todolistId]: [...state[action.payload.todolistId], { id: v1(), title: action.payload.title, isDone: false }] };
     }
+    case "CHANGE-TASK-STATUS": {
+      return { ...state, [action.payload.todolistId]: state[action.payload.todolistId].map(t => t.id === action.payload.taskId ? { ...t, isDone: action.payload.isDone } : t) };
+    }
+
+    case "CHANGE-TASK-TITLE": {
+      return { ...state, [action.payload.todolistId]: state[action.payload.todolistId].map(t => t.id === action.payload.taskId ? { ...t, title: action.payload.title } : t) };
+    }
 
     default:
       throw new Error("I don't understand this type");
@@ -30,10 +37,28 @@ export const addTaskAC = (payload: { title: string, todolistId: string; }) => {
   } as const;
 };
 
+export const changeTaskStatusAC = (payload: { taskId: string, isDone: boolean, todolistId: string; }) => {
+  return {
+    type: "CHANGE-TASK-STATUS",
+    payload
+  } as const;
+};
+
+export const changeTaskTitleAC = (payload: { taskId: string, title: string, todolistId: string; }) => {
+  return {
+    type: "CHANGE-TASK-TITLE",
+    payload
+  } as const;
+};
+
 
 // Actions types
 export type removeTaskActionType = ReturnType<typeof removeTaskAC>;
 
 export type addTaskActionType = ReturnType<typeof addTaskAC>;
 
-type ActionsType = removeTaskActionType | addTaskActionType;
+export type changeTaskStatusType = ReturnType<typeof changeTaskStatusAC>;
+
+export type changeTaskTitleType = ReturnType<typeof changeTaskTitleAC>;
+
+type ActionsType = removeTaskActionType | addTaskActionType | changeTaskStatusType | changeTaskTitleType;
